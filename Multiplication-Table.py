7b261008b06cecb4d1fd79aeab4f648016d81947
Juan Celaya <jcelayardz@gmail.com>
2021-02-11 16:55:11 +0100

    Do the multiplication table kata.

---
+++ Multiplication-Table.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08
+
+def multiplication_table(size):
+    mult_table = []
+    for row in range(1,size+1):
+        row_table = []
+        for column in range(1,size+1):
+            row_table.append(row*column)
+        mult_table.append(row_table)
+    return mult_table
+
+assert multiplication_table(3) == [[1,2,3],[2,4,6],[3,6,9]] , "Should be [[1,2,3],[2,4,6],[3,6,9]]"
+assert multiplication_table(2) == [[1, 2], [2, 4]] , "Should be [[1, 2], [2, 4]]"
