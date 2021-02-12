69ead4eef3dd6a17d6d07da628cc0eedd78b30b5
Juan Celaya <jcelayardz@gmail.com>
2021-02-12 15:21:17 +0100

    Do the ascii value kata

---
+++ ascii-value.py
@@ -0,0 +1,6 @@
+get_char = lambda character: chr(character)
+
+assert get_char(69) == 'E' , "Should be E"
+assert get_char(77) == 'M' , "Should be M"
+assert get_char(77) == 'M' , "Should be M"
+assert get_char(65) == 'A' , "Should be A"
